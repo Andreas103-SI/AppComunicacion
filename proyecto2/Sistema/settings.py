1923c1c06faaed1db1aff4acd33b1d6105703145
Andrea <andreasierra1223@gmail.com>
2025-03-06 08:49:40 +0100

    Elimina django_extensions de INSTALLED_APPS y cambia el código de idioma a español en settings.py

--- proyecto2/Sistema/settings.py
+++ proyecto2/Sistema/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'TaskFlow',
-    'django_extensions',
     
 ]
 
@@ -115,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
 TIME_ZONE = 'UTC'
 
