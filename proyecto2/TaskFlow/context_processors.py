55ac400a331c425b62c2d909b7e4ea691476a7dc
Andreas103-SI <andreasierra1223@gmail.com>
2025-03-19 21:59:10 +0100

    Ajustar .gitignore y añadir context processor para notificaciones

--- proyecto2/TaskFlow/context_processors.py
+++ proyecto2/TaskFlow/context_processors.py
@@ -2,6 +2,7 @@ from .models import Notificacion
 
 def notificaciones_no_leidas(request):
     if request.user.is_authenticated:
-        notificaciones = Notificacion.objects.filter(usuario=request.user, leida=False)
-        return {'notificaciones_no_leidas': notificaciones}
+        return {
+            'notificaciones_no_leidas': Notificacion.objects.filter(usuario=request.user, leida=False)
+        }
     return {'notificaciones_no_leidas': []}
